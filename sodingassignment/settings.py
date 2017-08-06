0be9cfc191d739f3a647c8cbfa9204c686bf8f30
Mohd Sheeraz <sheeraz@Sheeraz-Macbook-Pro.local>
2017-08-06 15:21:38 +0530

    Added Heroku Settings

--- sodingassignment/settings.py
+++ sodingassignment/settings.py
@@ -115,9 +115,19 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/'
-LOGIN_URL = '/login'
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
